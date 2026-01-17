f0772c3eb403c162bbd3f5edec774ba3093f4636
Bhavesh Shah <bhavesh6006@gmail.com>
2026-01-17 14:01:07 +0530

    Initial Set up

---
+++ .eslintrc.cjs
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    vitest: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier'
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react', '@typescript-eslint'],
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+  rules: {
+    'react/react-in-jsx-scope': 'off'
+  }
+}
